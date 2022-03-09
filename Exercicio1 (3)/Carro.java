69da83680e8cbc3f8ef09d863c79473f2a8ef618
lucasemanue <79729396+lucasemanue@users.noreply.github.com>
2022-03-08 21:45:15 -0300

    Add files via upload

---
+++ Exercicio1 (3)/Carro.java
@@ -0,0 +1,11 @@
+public class Carro{
+  private String modelo;
+  private short ano;
+  private String fabricante;
+  private String chassi;
+  private byte numportas;
+  private String cor;
+  private String codfipe;
+  private double precovenda;
+  
+}
